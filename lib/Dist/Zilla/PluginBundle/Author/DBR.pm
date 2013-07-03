0bd95c1faa8b6cce6bb3f2eab2ab930ba5a11243
Daniel <dbr@cpan.org>
2013-07-03 21:04:59 +0000

    Import of DBR/Dist-Zilla-PluginBundle-Author-DBR-0.840 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-PluginBundle-Author-DBR
    gitpan-cpan-version:      0.840
    gitpan-cpan-path:         DBR/Dist-Zilla-PluginBundle-Author-DBR-0.840.tar.gz
    gitpan-cpan-author:       DBR
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/PluginBundle/Author/DBR.pm
+++ lib/Dist/Zilla/PluginBundle/Author/DBR.pm
@@ -3,7 +3,7 @@ BEGIN {
   $Dist::Zilla::PluginBundle::Author::DBR::AUTHORITY = 'cpan:DBR';
 }
 {
-  $Dist::Zilla::PluginBundle::Author::DBR::VERSION = '0.830';
+  $Dist::Zilla::PluginBundle::Author::DBR::VERSION = '0.840';
 } # Make CPAN happy
 
 #  PODNAME: Dist::Zilla::PluginBundle::Author::DBR
@@ -82,7 +82,7 @@ Dist::Zilla::PluginBundle::Author::DBR - DBRs Dist::Zilla PluginBundle
 
 =head1 VERSION
 
-version 0.830
+version 0.840
 
 =head1 SYNOPSIS
 
